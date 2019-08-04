e561a064a0f05f3ab47782f3c1e550a92c442f5a
unknown <dbsdnwp1004@naver.com>
2019-08-04 12:53:06 +0900

    1

--- styled-native/App.js
+++ styled-native/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import styled from "styled-components";
+
+const Contatiner = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 100px;
+`;
+
+const Title = styled.Text`
+  font-weight: 600;
+  font-size: 32px;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Contatiner>
+      <Title>Open up App.js to start working on your app!</Title>
+    </Contatiner>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
