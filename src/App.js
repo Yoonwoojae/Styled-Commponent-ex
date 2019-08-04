1caac20e887b6154453b02b7929a6b797221b368
unknown <dbsdnwp1004@naver.com>
2019-08-04 12:42:15 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import theme from "./theme";
 
-function App() {
-  return <div className="App" />;
+createGlobalStyle`
+  body {
+    padding: 0;
+    margin:0;
+  }
+`;
+
+const Card = styled.div`
+  backgroud-color: red;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: pink;
+  ${Card} {
+    background-color: blue;
+  }
+`;
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Form />
+        </Container>
+      </ThemeProvider>
+    );
+  }
 }
+
+const Form = () => {
+  return (
+    <Card>
+      <Button>Hello</Button>
+    </Card>
+  );
+};
 
 export default App;
